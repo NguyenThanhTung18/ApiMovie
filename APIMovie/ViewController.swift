a1d323d91572612d569b0c19acf593cc4f63ca84
Tùng Thiện <tungthien@Tungs-MacBook-Pro.local>
2023-01-11 15:07:07 +0700

    fixbug

--- APIMovie/ViewController.swift
+++ APIMovie/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("Tung Beo")
         getAPI()
         config()
     }
